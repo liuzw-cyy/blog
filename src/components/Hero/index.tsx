da4f452ced8da596af43c1815b61ea5be2addea9
liuzw <liuzw1999@163.com>
2023-04-09 19:40:06 +0800

    docs: change background image

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -7,7 +7,7 @@ import { ThemeConfig } from '@docusaurus/preset-classic'
 
 import Link from '@docusaurus/Link'
 
-import HeroMain from './img/welcome.svg'
+import HeroMain from './img/image.svg'
 
 import JuejinIcon from '@site/static/svg/juejin.svg'
 import { Icon } from '@iconify/react'
